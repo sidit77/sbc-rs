73f19de716925e41d2960465b1ff5de24d5578c3
sidit77 <sidit77@gmail.com>
2024-05-20 14:48:11 +0200

    created a playback example

---
+++ examples/playback.rs
@@ -0,0 +1,57 @@
+use std::iter;
+use std::thread::sleep;
+use std::time::Duration;
+use anyhow::Context;
+use cpal::{default_host, SampleFormat};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use sbc_rs::Decoder;
+
+fn main() -> anyhow::Result<()> {
+    let host = default_host();
+    let device = host
+        .default_output_device()
+        .context("failed to find output device")?;
+
+    let config = device.supported_output_configs()?
+        .find(|config| config.sample_format() == SampleFormat::I16)
+        .context("failed to find output config")?
+        .with_max_sample_rate()
+        .config();
+
+    let file = std::fs::read("../bluefang/target/sbc/output.sbc")?;
+    let mut decoder = Decoder::new(file);
+    let mut source = iter::from_fn(move || decoder.next_frame())
+        .inspect(|frame| println!("decoded {} samples", frame.len()))
+        .flat_map(|frame| frame.into_iter())
+        .chain(iter::repeat(0));
+    //let mut source = iter::from_fn(move || decoder
+    //    .next_frame()
+    //    .map_err(|e| eprintln!("error decoding frame: {}", e))
+    //    .ok())
+    //    .flat_map(|frame| frame.data.into_iter())
+    //    .chain(iter::repeat(0));
+
+    //let mut source = (0u64..)
+    //    .map(|i| (i as f32 * (100.0 + 200.0 * f32::sin(i as f32 * 0.00001)) * 2.0 * std::f32::consts::PI / 44000.0).sin())
+    //    .map(|s| (s * 0.7 * i16::MAX as f32) as i16);
+
+    let stream = device.build_output_stream(
+        &config,
+        move |data: &mut [i16], _info| {
+            println!("playing {} samples", data.len());
+            data.into_iter().for_each(|d| *d = source.next().unwrap());
+        },
+        move |err| {
+            eprintln!("an error occurred on the output stream: {}", err);
+        },
+        None,
+    )?;
+
+    stream.play()?;
+
+    sleep(Duration::from_secs(30));
+
+    stream.pause()?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,81 @@
-mod bits;
-mod sbc;
+use std::mem::zeroed;
+use crate::sbc::{sbc_decode, sbc_get_frame_size, SBC_MODE_MONO, sbc_probe, sbc_reset, sbc_t};
 
-const SBC_HEADER_SIZE: usize = 4;
-const SBC_PROBE_SIZE: usize = SBC_HEADER_SIZE;
+pub mod bits;
+pub mod sbc;
 
-const SBC_MAX_SUBBANDS: usize = 8;
-const SBC_MAX_BLOCKS: usize = 16;
-const SBC_MAX_SAMPLES: usize = SBC_MAX_BLOCKS * SBC_MAX_SUBBANDS;
+pub const SBC_HEADER_SIZE: usize = 4;
+pub const SBC_PROBE_SIZE: usize = SBC_HEADER_SIZE;
 
-const SBC_MSBC_SAMPLES: usize = 120;
-const SBC_MSBC_SIZE: usize = 57;
+pub const SBC_MAX_SUBBANDS: usize = 8;
+pub const SBC_MAX_BLOCKS: usize = 16;
+pub const SBC_MAX_SAMPLES: usize = SBC_MAX_BLOCKS * SBC_MAX_SUBBANDS;
+
+pub const SBC_MSBC_SAMPLES: usize = 120;
+pub const SBC_MSBC_SIZE: usize = 57;
+
+pub struct Decoder {
+    buffer: Vec<u8>,
+    index: usize,
+    sbc: Box<sbc_t>
+}
+
+unsafe impl Send for Decoder {}
+
+impl Decoder {
+    pub fn new(data: Vec<u8>) -> Self {
+        let mut sbc: Box<sbc_t> = unsafe { Box::new(zeroed()) };
+        unsafe { sbc_reset(sbc.as_mut()) };
+        Self { buffer: data, index: 0, sbc }
+    }
+
+    pub fn next_frame(&mut self) -> Option<Vec<i16>> {
+        let remaining = &self.buffer[self.index..];
+        if remaining.len() < SBC_PROBE_SIZE { return None; }
+        assert_eq!(remaining.len().min(SBC_PROBE_SIZE), SBC_PROBE_SIZE);
+        unsafe {
+            let mut frame = zeroed();
+            assert_eq!(sbc_probe(remaining.as_ptr().cast(), &mut frame), 0);
+            assert_ne!(frame.mode, SBC_MODE_MONO);
+            let frame_size = sbc_get_frame_size(&frame) as usize;
+
+            if remaining.len() < frame_size { return None; }
+            assert_eq!(remaining.len().min(frame_size), frame_size);
+
+            let mut pcm: Vec<i16> = vec![0; (2 * frame.nblocks * frame.nsubbands) as usize];
+
+            assert_eq!(sbc_decode(
+                &mut *self.sbc,
+                remaining.as_ptr().cast(),
+                remaining.len() as _,
+                &mut frame,
+                pcm.as_mut_ptr(),
+                2,
+                pcm.as_mut_ptr().add(1),
+                2
+            ), 0);
+            self.index += frame_size;
+
+            Some(pcm)
+        }
+
+    }
+
+}
+
 
 #[cfg(test)]
 mod tests {
-    use std::mem::zeroed;
-    use crate::sbc::{sbc_decode, sbc_get_frame_size, sbc_probe, sbc_reset};
     use super::*;
 
     #[test]
     fn it_works() {
         let data = std::fs::read("../bluefang/target/sbc/output.sbc").unwrap();
-        let mut index = 0;
-        unsafe {
-            let mut sbc = zeroed();
-            sbc_reset(&mut sbc);
-
-            let mut pcm = [0i16; 2 * SBC_MAX_SAMPLES];
-
-            let mut number_of_frames = 0;
-            while index < data.len() {
-                let remaining = &data[index..];
-                assert_eq!(remaining.len().min(SBC_PROBE_SIZE), SBC_PROBE_SIZE);
-                let mut frame = zeroed();
-                assert_eq!(sbc_probe(remaining.as_ptr().cast(), &mut frame), 0);
-                let frame_size = sbc_get_frame_size(&frame) as usize;
-                assert_eq!(remaining.len().min(frame_size), frame_size);
-
-                assert_eq!(sbc_decode(
-                    &mut sbc,
-                    remaining.as_ptr().cast(),
-                    remaining.len() as _,
-                    &mut frame,
-                    pcm.as_mut_ptr(),
-                    2,
-                    pcm.as_mut_ptr().add(1),
-                    2
-                ), 0);
-
-                index += frame_size;
-                number_of_frames += 1;
-            }
-
-            println!("Decoded {} frames", number_of_frames);
+        let mut decoder = Decoder::new(data);
+        let mut count = 0;
+        while let Some(_) = decoder.next_frame() {
+            count += 1;
         }
+        println!("Decoded {} frames", count);
     }
 }
