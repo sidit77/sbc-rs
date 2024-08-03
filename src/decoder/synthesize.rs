93601207dbe28124d719dcaec525d6f399e3bbaf
sidit77 <sidit77@gmail.com>
2024-08-03 10:11:16 +0200

    ported dct4

--- src/decoder/synthesize.rs
+++ src/decoder/synthesize.rs
@@ -1,136 +1,62 @@
 use std::ffi::c_int;
+use std::slice;
 use crate::raw::{int16_t, sbc_dstate};
 
 #[inline]
-unsafe fn dct4(
-    in_0: *const int16_t,
-    scale: c_int,
-    out0: *mut [int16_t; 10],
-    out1: *mut [int16_t; 10],
-    idx: c_int,
-) {
-    static mut cos8: [int16_t; 4] = [
-        8192 as c_int as int16_t,
-        7568 as c_int as int16_t,
-        5793 as c_int as int16_t,
-        3135 as c_int as int16_t,
-    ];
-    let s03: int16_t = ((*in_0.offset(0 as c_int as isize) as c_int
-        + *in_0.offset(3 as c_int as isize) as c_int) >> 1 as c_int)
-        as int16_t;
-    let d03: int16_t = ((*in_0.offset(0 as c_int as isize) as c_int
-        - *in_0.offset(3 as c_int as isize) as c_int) >> 1 as c_int)
-        as int16_t;
-    let s12: int16_t = ((*in_0.offset(1 as c_int as isize) as c_int
-        + *in_0.offset(2 as c_int as isize) as c_int) >> 1 as c_int)
-        as int16_t;
-    let d12: int16_t = ((*in_0.offset(1 as c_int as isize) as c_int
-        - *in_0.offset(2 as c_int as isize) as c_int) >> 1 as c_int)
-        as int16_t;
-    let mut a0: c_int = (s03 as c_int - s12 as c_int)
-        * cos8[2 as c_int as usize] as c_int;
-    let mut b1: c_int = -(s03 as c_int + s12 as c_int)
-        << 13 as c_int;
-    let mut a1: c_int = d03 as c_int
-        * cos8[3 as c_int as usize] as c_int
-        - d12 as c_int * cos8[1 as c_int as usize] as c_int;
-    let mut b0: c_int = -(d03 as c_int)
-        * cos8[1 as c_int as usize] as c_int
-        - d12 as c_int * cos8[3 as c_int as usize] as c_int;
-    let shr: c_int = 12 as c_int + scale;
-    a0 = (a0 + ((1 as c_int) << (shr - 1 as c_int))) >> shr;
-    b0 = (b0 + ((1 as c_int) << (shr - 1 as c_int))) >> shr;
-    a1 = (a1 + ((1 as c_int) << (shr - 1 as c_int))) >> shr;
-    b1 = (b1 + ((1 as c_int) << (shr - 1 as c_int))) >> shr;
-    (*out0
-        .offset(
-            0 as c_int as isize,
-        ))[idx
-        as usize] = (if a0 > 32767 as c_int {
-        32767 as c_int
-    } else if a0 < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        a0
-    }) as int16_t;
-    (*out0
-        .offset(
-            3 as c_int as isize,
-        ))[idx
-        as usize] = (if -a1 > 32767 as c_int {
-        32767 as c_int
-    } else if -a1 < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        -a1
-    }) as int16_t;
-    (*out0
-        .offset(
-            1 as c_int as isize,
-        ))[idx
-        as usize] = (if a1 > 32767 as c_int {
-        32767 as c_int
-    } else if a1 < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        a1
-    }) as int16_t;
-    (*out0
-        .offset(
-            2 as c_int as isize,
-        ))[idx
-        as usize] = (if 0 as c_int > 32767 as c_int {
-        32767 as c_int
-    } else if (0 as c_int) < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        0 as c_int
-    }) as int16_t;
-    (*out1
-        .offset(
-            0 as c_int as isize,
-        ))[idx
-        as usize] = (if -a0 > 32767 as c_int {
-        32767 as c_int
-    } else if -a0 < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        -a0
-    }) as int16_t;
-    (*out1
-        .offset(
-            3 as c_int as isize,
-        ))[idx
-        as usize] = (if b0 > 32767 as c_int {
-        32767 as c_int
-    } else if b0 < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        b0
-    }) as int16_t;
-    (*out1
-        .offset(
-            1 as c_int as isize,
-        ))[idx
-        as usize] = (if b0 > 32767 as c_int {
-        32767 as c_int
-    } else if b0 < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        b0
-    }) as int16_t;
-    (*out1
-        .offset(
-            2 as c_int as isize,
-        ))[idx
-        as usize] = (if b1 > 32767 as c_int {
-        32767 as c_int
-    } else if b1 < -(32767 as c_int) - 1 as c_int {
-        -(32767 as c_int) - 1 as c_int
-    } else {
-        b1
-    }) as int16_t;
+fn sat16(v: i32) -> i16 {
+    v.max(i16::MIN as i32).min(i16::MAX as i32) as i16
 }
+
+#[inline]
+fn dct4(data: &[i16], scale: i32, out0: &mut [[i16; 10]; 8], out1: &mut [[i16; 10]; 8], idx: usize) {
+    /* cos(i*pi/8)  for i = [0;3], in fixed 0.13 */
+    const COS8: [i32; 4] = [8192, 7568, 5793, 3135];
+
+    /* --- DCT of subbands samples ---
+     *          ___
+     *          \
+     *   u[k] = /__  h(k,i) * s(i) , i = [0;n-1]  k = [0;2n-1]
+     *           i
+     *
+     *   With  n the number of subbands (4)
+     *         h(k,i) = cos( (i + 1/2) (k + n/2) pi/n )
+     *
+     * Note :
+     *
+     *     h( 2, i) =  0 et h( n-k,i) = -h(k,i)   , k = [0;n/2-1]
+     *     h(12, i) = -1 et h(2n-k,i) =  h(n+k,i) , k = [1;n/2-1]
+     *
+     * To assist the windowing step, the 2 halves are stored in 2 buffers.
+     * After scaling of coefficients, the result is saturated on 16 bits. */
+    let s03 = (data[0] as i32 + data[3] as i32) >> 1;
+    let d03 = (data[0] as i32 - data[3] as i32) >> 1;
+    let s12 = (data[1] as i32 + data[2] as i32) >> 1;
+    let d12 = (data[1] as i32 - data[2] as i32) >> 1;
+
+    let mut a0 = (s03 - s12 ) * COS8[2];
+    let mut b1 = (-(s03 + s12 )) << 13;
+    let mut a1 =  d03 * COS8[3] - d12 * COS8[1];
+    let mut b0 = -d03 * COS8[1] - d12 * COS8[3];
+
+    let shr = 12 + scale;
+
+    a0 = (a0 + (1 << (shr-1))) >> shr;
+    b0 = (b0 + (1 << (shr-1))) >> shr;
+    a1 = (a1 + (1 << (shr-1))) >> shr;
+    b1 = (b1 + (1 << (shr-1))) >> shr;
+
+    out0[0][idx] = sat16( a0);
+    out0[1][idx] = sat16( a1);
+    out0[2][idx] = sat16(  0);
+    out0[3][idx] = sat16(-a1);
+
+    out1[0][idx] = sat16(-a0);
+    out1[1][idx] = sat16( b0);
+    out1[2][idx] = sat16( b1);
+    out1[3][idx] = sat16( b0);
+
+}
+
 #[inline]
 unsafe fn dct8(
     in_0: *const int16_t,
@@ -576,12 +502,11 @@ unsafe fn sbc_synthesize_4_c(
     };
     let odd: c_int = dct_idx & 1 as c_int;
     dct4(
-        in_0,
+        slice::from_raw_parts(in_0, 4),
         scale,
-        ((*state).v[odd as usize]).as_mut_ptr(),
-        ((*state).v[(odd == 0) as c_int as usize]).as_mut_ptr(),
-        dct_idx,
-    );
+        &mut (*state).v[odd as usize],
+        &mut (*state).v[(odd == 0) as usize],
+        dct_idx as usize);
     apply_window(
         ((*state).v[odd as usize]).as_mut_ptr() as *const [int16_t; 10],
         4 as c_int,
